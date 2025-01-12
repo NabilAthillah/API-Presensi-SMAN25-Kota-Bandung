a85a1175ce0afd118d9332979de70305bec2356c
Pika <133586196+NabilAthillah@users.noreply.github.com>
2025-01-12 19:05:12 +0700

    Add Parent Login API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parents;
 use App\Models\Students;
 use App\Models\Teachers;
 use Hash;
@@ -54,7 +55,48 @@ class AuthController extends Controller
                 ], 401);
             }
         }
-        
+
+        return response()->json([
+            'message' => 'User tidak ditemukan'
+        ], 404);
+    }
+
+    public function loginParent(Request $request) {
+        if(!$request->email) {
+            return response()->json([
+                'message' => 'Email tidak boleh kosong',
+            ], 403);
+        }
+
+        if(!$request->password) {
+            return response()->json([
+                'message' => 'Password tidak boleh kosong',
+            ], 403);
+        }
+
+        $token = Str::random(60);
+
+        $parent = Parents::where('email', $request->email)->first();
+        if($parent) {
+            if($parent->verified_status == 'verified') {
+                if(Hash::check($request->password, $parent->password)) {
+                    return response()->json([
+                        'message' => 'Login berhasil, selamat datang ' . $parent->name . '!',
+                        'role' => 'parent',
+                        'token' => $token
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'message' => 'Password salah',
+                    ], 401);
+                }
+            } else {
+                return response()->json([
+                    'message' => 'Email ini belum diverifikasi',
+                ], 401);
+            }
+        }
+
         return response()->json([
             'message' => 'User tidak ditemukan'
         ], 404);
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function() {
     Route::controller(AuthController::class)->group(function () {
         Route::post('/login', 'login');
+        Route::post('/login/parent', 'loginParent');
     });
 });
 
